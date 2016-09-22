7f0d0d73e77c2c275348cdcccf9a71cd7e7eff0d
Kyle Constance <kconst@gmail.com>
2016-09-22 01:14:51 +0000

    initial commit

---
+++ src/PlatesSelect.js
@@ -0,0 +1,44 @@
+import React, { Component, PropTypes } from 'react'
+import classnames from 'classnames'
+import BarbellWeights from './BarbellWeights.js';
+
+class PlatesSelect extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            weight: 20
+        }
+    }
+
+    componentWillMount() {
+        this.setState({
+           plates: this.props.isImperial ? [2.5, 5, 10, 25, 45] : [2.5, 5, 10, 15, 20, 25]
+        });
+    }
+
+    increaseWeight(weight) {
+       this.setState({
+           weight: this.state.weight += weight
+       });
+    }
+
+    render() {
+        return (
+            <section className="PlatesSelect">
+                <h3>{ this.state.weight }</h3>
+                <BarbellWeights useImperial={ false } weight={ this.state.weight }/>
+                <ul className="plate-list">
+                    {this.state.plates.map((plate, i) =>
+                        <li key={i} className={ 'plate-' + (plate + '').replace('.', 'p') } onClick={ () => this.setState({ weight: this.state.weight += (plate * 2) }) }>{ plate }</li>
+                    )}
+                </ul>
+            </section>
+        );
+    }
+}
+
+PlatesSelect.propTypes = {
+    
+};
+
+export default PlatesSelect
